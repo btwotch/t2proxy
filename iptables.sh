9903b6be12ce692a2b945fa46f9632ac8f46b7b5
Steven Falken <btwotch+git@gmail.com>
2020-10-11 18:59:48 +0200

    iptables: rename chain

--- iptables.sh
+++ iptables.sh
@@ -6,24 +6,24 @@ then
 	exit 1
 fi
 
-iptables -t nat -N REDSOCKS
-ip6tables -t nat -N REDSOCKS
+iptables -t nat -N TTWO
+ip6tables -t nat -N TTWO
 
 set -euo pipefail
 
-iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
-iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
-iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
-iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
-iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
-iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
-iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
-iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
+iptables -t nat -A TTWO -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A TTWO -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A TTWO -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A TTWO -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A TTWO -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A TTWO -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A TTWO -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A TTWO -d 240.0.0.0/4 -j RETURN
 
 # Anything else should be redirected to port 12345
-iptables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 3129
+iptables -t nat -A TTWO -p tcp -j REDIRECT --to-ports 3129
 
-ip6tables -t nat -A REDSOCKS -p tcp -j REDIRECT --to-ports 3129
+ip6tables -t nat -A TTWO -p tcp -j REDIRECT --to-ports 3129
 # Any tcp connection made by `luser' should be redirected.
-iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner $1 -j REDSOCKS
-ip6tables -t nat -A OUTPUT -p tcp -m owner --uid-owner $1 -j REDSOCKS
+iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner $1 -j TTWO
+ip6tables -t nat -A OUTPUT -p tcp -m owner --uid-owner $1 -j TTWO
--- iptables_clean.sh
+++ iptables_clean.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-iptables -F REDSOCKS -t nat
-iptables -X REDSOCKS -t nat
+iptables -F TTWO -t nat
+iptables -X TTWO -t nat
 
-ip6tables -F REDSOCKS -t nat
-ip6tables -X REDSOCKS -t nat
+ip6tables -F TTWO -t nat
+ip6tables -X TTWO -t nat
 
 #iptables -P INPUT ACCEPT
 #iptables -P FORWARD ACCEPT
