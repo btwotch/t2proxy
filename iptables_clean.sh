d1b588750cc537a89488db6dae3edd7bd0fd177d
Steven Falken <btwotch+git@gmail.com>
2020-10-14 20:20:33 +0200

    iptables: improve cleanup script

--- iptables_clean.sh
+++ iptables_clean.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+        echo "No arguments supplied"
+        exit 1
+fi
+
+set -euo pipefail
 
 iptables -F TTWO -t nat
 iptables -X TTWO -t nat
@@ -6,10 +14,4 @@ iptables -X TTWO -t nat
 ip6tables -F TTWO -t nat
 ip6tables -X TTWO -t nat
 
-#iptables -P INPUT ACCEPT
-#iptables -P FORWARD ACCEPT
-#iptables -P OUTPUT ACCEPT
-#iptables -t nat -F
-#iptables -t mangle -F
-#iptables -F
-#iptables -X
+iptables -D OUTPUT -p tcp -m owner --uid-owner $1 -j TTWO -t nat
